b49d7914927371ba68fb1509bdc4d90e5120afa3
Victoria Muñoz <victoriamunozrobles@gmail.com>
2026-04-02 11:05:42 -0300

    feat: crea vista del detalle de los modulos para el perfil de estudiante

--- otec-primavera/src/main/java/cl/td/otec_primavera/controlador/PortalEstudianteController.java
+++ otec-primavera/src/main/java/cl/td/otec_primavera/controlador/PortalEstudianteController.java
@@ -1,11 +1,16 @@
 package cl.td.otec_primavera.controlador;
 
 import cl.td.otec_primavera.modelo.Estudiante;
+import cl.td.otec_primavera.modelo.Evaluacion;
+import cl.td.otec_primavera.modelo.Modulo;
 import cl.td.otec_primavera.servicio.EstudianteService;
 import cl.td.otec_primavera.servicio.EvaluacionService;
+import cl.td.otec_primavera.servicio.ModuloService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 import java.security.Principal;
 
 @Controller
@@ -13,10 +18,13 @@ public class PortalEstudianteController {
 
     private final EstudianteService estudianteService;
     private final EvaluacionService evaluacionService;
+    private final ModuloService moduloService;
 
-    public PortalEstudianteController(EstudianteService estudianteService, EvaluacionService evaluacionService) {
+    public PortalEstudianteController(EstudianteService estudianteService, EvaluacionService evaluacionService,
+            ModuloService moduloService) {
         this.estudianteService = estudianteService;
         this.evaluacionService = evaluacionService;
+        this.moduloService = moduloService;
     }
 
     @GetMapping("/mi-portal")
@@ -35,5 +43,28 @@ public class PortalEstudianteController {
         }
 
         return "mi-portal";
+    }
+
+    @GetMapping("/mi-portal/modulo/{idModulo}")
+    public String verDetalleModulo(@PathVariable("idModulo") Integer idModulo, Principal principal, Model model) {
+        String email = principal.getName();
+        Estudiante estudiante = estudianteService.obtenerEstudiantePorEmail(email);
+
+        if (estudiante == null || estudiante.getCurso() == null) {
+            return "redirect:/mi-portal";
+        }
+
+        Modulo modulo = moduloService.obtenerModuloPorId(idModulo);
+
+        if (!modulo.getCurso().getIdCurso().equals(estudiante.getCurso().getIdCurso())) {
+            return "redirect:/mi-portal";
+        }
+
+        Evaluacion miNota = evaluacionService.obtenerNotaEspecifica(estudiante.getIdEstudiante(), idModulo);
+
+        model.addAttribute("modulo", modulo);
+        model.addAttribute("miNota", miNota);
+
+        return "modulo-detalle";
     }
 }
